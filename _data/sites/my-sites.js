57674c406df3f6d42d1300ad25dc2602a23dde94
Karsten Dambekalns <karsten@dambekalns.de>
2023-07-14 12:35:57 +0200

    Enable tests…

--- _data/sites/my-sites.js
+++ _data/sites/my-sites.js
@@ -2,8 +2,7 @@ module.exports = {
 	name: "My sites, somehow…", // optional, falls back to object key
 	description: "Sites I care about",
 	// skip if localhost
-	// skip if this is a new fork of the speedlify (not Zach’s)
-	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	skip: !process.env.CONTEXT,
 	options: {
 		frequency: 60 * 24, // 24 hours
 		// Use "run" if the sites don’t share assets on the same origin
